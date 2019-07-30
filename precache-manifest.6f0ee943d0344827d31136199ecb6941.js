0a1ffa45b944cca8b08c655fc748777f9cd2559e
Ryan Birch <ryan.birch@gmail.com>
2019-07-29 23:31:37 -0400

    Updates

---
+++ precache-manifest.6f0ee943d0344827d31136199ecb6941.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4b9f0c1a9ce34e7537730cdb61d6b41",
+    "url": "/react-memory-game/index.html"
+  },
+  {
+    "revision": "c7cac56b1732d7a29c6a",
+    "url": "/react-memory-game/static/css/main.8bbfe71e.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/react-memory-game/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "c7cac56b1732d7a29c6a",
+    "url": "/react-memory-game/static/js/main.1fa162ea.chunk.js"
+  },
+  {
+    "revision": "e8b71f681a914c951703",
+    "url": "/react-memory-game/static/js/runtime~main.83a7a993.js"
+  },
+  {
+    "revision": "9a26e562e4f2bbc9d18110c408a755d1",
+    "url": "/react-memory-game/static/media/sb1.9a26e562.jpg"
+  },
+  {
+    "revision": "905fd817e706951f3c38b164324ec025",
+    "url": "/react-memory-game/static/media/sb10.905fd817.webp"
+  },
+  {
+    "revision": "33be73d05eb0e5a3e2d99844e5778497",
+    "url": "/react-memory-game/static/media/sb11.33be73d0.jpg"
+  },
+  {
+    "revision": "333890fee7175e7e0617853d5020ae44",
+    "url": "/react-memory-game/static/media/sb12.333890fe.jpg"
+  },
+  {
+    "revision": "b319998a08e03b895adca95fb889f02f",
+    "url": "/react-memory-game/static/media/sb3.b319998a.jpg"
+  },
+  {
+    "revision": "b40219b0e854f67221479a0d89909f84",
+    "url": "/react-memory-game/static/media/sb4.b40219b0.jpg"
+  },
+  {
+    "revision": "c9d3c3eea7c5505558b4e08226b541d9",
+    "url": "/react-memory-game/static/media/sb6.c9d3c3ee.jpg"
+  },
+  {
+    "revision": "eabf8bfce89ccefd0c6abe4147fa98b3",
+    "url": "/react-memory-game/static/media/sb7.eabf8bfc.jpg"
+  },
+  {
+    "revision": "beed143093325cbdc437e490dcea6bc3",
+    "url": "/react-memory-game/static/media/sb8.beed1430.jpg"
+  },
+  {
+    "revision": "496162e74da187571bd63918aaed08ec",
+    "url": "/react-memory-game/static/media/sb9.496162e7.png"
+  }
+]);
